92409a4adfe321e0bdd991f4148438308c2bd490
unknown <emranulfarhad@gmail.com>
2022-06-12 01:51:59 +0600

    code clear console block

--- src/Shared/Nav.js
+++ src/Shared/Nav.js
@@ -15,9 +15,7 @@ const Nav = () => {
 
                     <NavLink to='/' >
                         <div class="md:p-5 p-3 delay-75 duration-500 ease-in-out transform hover:scale-125 hover:-rotate-12">
-                            {/* <svg xmlns="http://www.w3.org/2000/svg" class="text-black h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-                            </svg> */}
+                            
                             <FcHome className=' h-6 w-6'></FcHome>
                         </div>
                     </NavLink>
